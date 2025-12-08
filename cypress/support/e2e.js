8233d9a69204a50c89236220a1fd468fcc495152
Weronika Majewska <>
2025-12-08 18:51:03 +0100

    pipeline fix 5

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -14,6 +14,7 @@
 // ***********************************************************
 
 // Handle uncaught exceptions
+// eslint-disable-next-line no-unused-vars
 Cypress.on('uncaught:exception', (err, runnable) => {
   // returning false here prevents Cypress from
   // failing the test
